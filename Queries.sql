5f5c5814c78ce376fcbfca888dc47f356e3b9545
Anju Saini <110930875+anju0422@users.noreply.github.com>
2023-08-19 11:16:27 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -48,4 +48,78 @@ ORDER BY Emp_name;
 /* Q4. Write a query to find the name (first_name, last_name) and the salary of the employees who have a higher salary 
        than the employee whose last_name='Bull'. */
 
+SELECT 
+CONCAT(first_name," ", last_name) AS Emp_name,
+salary 
+FROM Employees
+WHERE salary > (SELECT 
+                salary
+                FROM Employees 
+                WHERE last_name = "Bull");
+
+/* Q5. Write a query to find the name (first_name, last_name) of the employees who have a manager working in a US-based department. */
+
+SELECT 
+e.first_name,
+e.last_name
+FROM Employees e
+JOIN Employees m 
+ON e.manager_id = m.employee_id
+JOIN Departments d 
+ON m.department_id = d.department_id
+JOIN Locations l 
+ON d.location_id = l.location_id  
+WHERE (l.country_id) = "US";
+
+/* Q6.  Write a query to find the name (first_name, last_name), and salary of the employees whose salary is 
+  greater than their department's average salary.*/
+
+WITH CTE AS(
+SELECT
+first_name,
+last_name, 
+department_id,
+AVG(salary) OVER (PARTITION BY department_id) AS avg_sal,
+salary 
+FROM employees
+) 
+    SELECT 
+    first_name,
+    last_name,
+    salary
+    FROM CTE 
+    WHERE salary > avg_sal;
+
+/* Q7. Write a query to find the name (first_name, last_name), and salary of the employees who earn more than 
+       the average salary and work in any of the IT departments. */
+
+ SELECT 
+   first_name,
+   last_name,
+   salary
+   FROM employees e 
+   JOIN departments d 
+   ON e.department_id = d.department_id
+   WHERE salary > (SELECT AVG(salary) FROM employees) 
+   AND d.department_name LIKE "IT%";
+
+/* Q8.  Write a query to find the name (first_name, last_name), and salary of the employees who earn the same salary as 
+        the minimum salary for all departments. */
+
+   WITH CTE AS( 
+		SELECT 
+		first_name,
+		last_name,
+		department_id,
+		salary,
+		MIN(salary) OVER (PARTITION BY department_id) AS min_sal
+		FROM employees
+	) 
+        SELECT 
+        first_name,
+        last_name,
+        salary
+        FROM CTE 
+        WHERE salary = min_sal;
+
 
